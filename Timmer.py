d41480e6922c2cde857b69e915f0525864715769
Anshugupta632 <anshugupta3592@gmail.com>
2025-12-22 23:39:26 -1200

    Add timer script and remove image file
    Added Timmer.py, a countdown timer script that beeps when time is up. Removed insta.jpg image file.

---
+++ Timmer.py
@@ -0,0 +1,14 @@
+import time
+import winsound  
+
+my_time = int(input("Enter time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = (x // 60) % 60
+    hours = (x // 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}", end="\r")
+    time.sleep(1)
+
+print("\nTime's up!")
+winsound.Beep(1000, 500)  
